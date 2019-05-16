44ddd370a4d41f873ac85882dd60ccd745c336ec
Thwarikh Anvar T S <48545060+thwarikh@users.noreply.github.com>
2019-05-16 15:53:05 +0530

    Update gDrive.py

--- plugins/gDrive.py
+++ plugins/gDrive.py
@@ -33,7 +33,7 @@ from pydrive.auth import GoogleAuth
 @pyrogram.Client.on_message(pyrogram.Filters.command(["gauth"]))
 def g_auth(bot, update):
     TRChatBase(update.from_user.id, update.text, "gauth")
-    if str(update.from_user.id) in Config.SUPER7X_DLBOT_USERS:
+    if str(update.from_user.id) not in Config.SUPER7X_DLBOT_USERS:
         bot.send_message(
             chat_id=update.chat.id,
             text=Translation.NOT_AUTH_USER_TEXT,
@@ -53,7 +53,7 @@ def g_auth(bot, update):
 @pyrogram.Client.on_message(pyrogram.Filters.command(["gsetup"]))
 def g_setup(bot, update):
     TRChatBase(update.from_user.id, update.text, "gsetup")
-    if str(update.from_user.id) in Config.SUPER7X_DLBOT_USERS:
+    if str(update.from_user.id) not in Config.SUPER7X_DLBOT_USERS:
         bot.send_message(
             chat_id=update.chat.id,
             text=Translation.NOT_AUTH_USER_TEXT,

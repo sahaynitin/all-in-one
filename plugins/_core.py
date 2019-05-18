de06b68dc1222ea2641e2910be63d34309cef729
Thwarikh Anvar T S <48545060+thwarikh@users.noreply.github.com>
2019-05-18 12:18:49 +0530

    Update _core.py

--- plugins/_core.py
+++ plugins/_core.py
@@ -25,7 +25,7 @@ else:
 from translation import Translation
 
 import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.INFO)
 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes

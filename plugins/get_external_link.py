f9539f5ef37441a3d9f456df809fb0d7faff092b
Thwarikh Anvar T S <48545060+thwarikh@users.noreply.github.com>
2019-05-21 00:37:50 +0530

    Update get_external_link.py

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -84,10 +84,10 @@ def get_link(bot, update):
             max_days = 0
         else:
             url = "https://mirrorace.com/api/v1/file/upload/{}".format(upload_name)
-            step_one_auth_params = {
-            "api_key": Config.MIRROR_ACE_API_KEY,
-            "api_token": Config.MIRROR_ACE_API_TOKEN
-        }
+            
+            api_key = Config.MIRROR_ACE_API_KEY,
+            api_token = Config.MIRROR_ACE_API_TOKEN
+       
         max_days = "3"
             command_to_exec = [
                 "curl",

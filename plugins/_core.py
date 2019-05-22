ad0d35c8db13d69bb8962a6384cc301e26ac842d
Thwarikh Anvar T S <48545060+thwarikh@users.noreply.github.com>
2019-05-23 03:32:27 +0530

    Update _core.py

--- plugins/_core.py
+++ plugins/_core.py
@@ -4,7 +4,7 @@
 
 # the logging things
 import logging
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 

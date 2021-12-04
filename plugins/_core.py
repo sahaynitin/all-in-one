d0c15c2951bf0dbf5f581f0cf4eced70bcfad925
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-04 13:31:16 +0530

    Update _core.py

--- plugins/_core.py
+++ plugins/_core.py
@@ -215,10 +215,7 @@ def echo(bot, update):
 
 @pyrogram.Client.on_callback_query()
 def button(bot, update):
-    return
-    cb_data = update.data.decode("UTF-8")
-    if cb_data.find("|") == -1:
-        return
+    cb_data = update.data.decode("
     tg_send_type, youtube_dl_format, youtube_dl_ext = cb_data.split("|")
     thumb_image_path = Config.DOWNLOAD_LOCATION + \
         "/" + str(update.from_user.id) + ".jpg"
